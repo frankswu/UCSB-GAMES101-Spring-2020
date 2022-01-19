30db1eda0b0832cbab36cbc6216d833d18f91e76
Liu Deyuan <liudeyuan2021@gmail.com>
2022-01-20 02:36:33 +0800

    hw8: part1

--- hw8/src/rope.cpp
+++ hw8/src/rope.cpp
@@ -12,11 +12,19 @@ namespace CGL {
     Rope::Rope(Vector2D start, Vector2D end, int num_nodes, float node_mass, float k, vector<int> pinned_nodes)
     {
         // TODO (Part 1): Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
+        Vector2D dis = (end - start) / (num_nodes - 1);
+        for (int i = 0; i < num_nodes; ++i) {
+            masses.push_back(new Mass(start + i * dis, node_mass, false));
+        }
+
+        for (int i = 0; i < num_nodes-1; ++i) {
+            springs.push_back(new Spring(masses[i], masses[i+1], k));
+        }
 
 //        Comment-in this part when you implement the constructor
-//        for (auto &i : pinned_nodes) {
-//            masses[i]->pinned = true;
-//        }
+       for (auto &i : pinned_nodes) {
+           masses[i]->pinned = true;
+       }
     }
 
     void Rope::simulateEuler(float delta_t, Vector2D gravity)
